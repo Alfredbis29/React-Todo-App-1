ca1e2a1c8589910460d9553f6914ff371e4ba1a2
Alfred <kalumunabisimwa5@gmail.com>
2022-01-12 12:31:31 +0200

    create jsx file

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/react-app.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
