e1b17f9d31fd7366c638a26084b2f12d66089d07
Alfred <kalumunabisimwa5@gmail.com>
2022-01-20 20:44:43 +0200

    install tailwindcss

--- react-app/src/component/Counter.js
+++ react-app/src/component/Counter.js
@@ -3,6 +3,10 @@ const Counter = () => {
         <div>
             <h1>Hello there and welcome</h1>
             <small>This is just a notice</small>
+            <form>
+                <input type="text" placeholder="Enter your mail"/>
+                <input type="text" placeholder="Enter your mail"/>
+            </form>
         </div>
     )
 }
