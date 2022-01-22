4d4e48146b698849fb15ddd7f7845815510570d6
Tresor <tresor.moise2001@gmail.com>
2022-01-22 12:48:30 +0200

    install react-redux

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,17 +1,15 @@
 const Counter = () => {
-    return (
-        <div>
-            <h1>Hello there and welcome</h1>
-            <small>This is just a notice</small>
-            <h1 className="text-3xl font-bold underline">
-                 Hello world!
-    </h1>
-            <form>
-                <input type="text" placeholder="Enter your mail"/>
-                <input type="text" placeholder="Enter your mail"/>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Hello there and welcome</h1>
+      <small>This is just a notice</small>
+      <h1 className="text-3xl text-indigo-600 font-bold underline">Hello world!</h1>
+      <form>
+        <input type="text" placeholder="Enter your mail" />
+        <input type="text" placeholder="Enter your mail" />
+      </form>
+    </div>
+  );
+};
 
 export default Counter;
