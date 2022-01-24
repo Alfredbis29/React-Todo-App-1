3570b3eaaf2ca4e50f2412092e02c9fdbb01d68f
Alfred <kalumunabisimwa5@gmail.com>
2022-01-24 18:24:20 +0200

    Add forms

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -3,7 +3,7 @@ const Counter = () => {
     <div>
       <h1>Hello there and welcome</h1>
       <small>This is just a notice</small>
-      <h1 className="text-3xl font-bold text-yellow-300">Hello world!</h1>
+      {/* <h1 className="text-3xl font-bold text-yellow-300">Hello world!</h1> */}
       <button
         onClick={console.log("Start your store")}
         className="border-solid border-black bg-yellow-600 p-2 rounded ml-4"
@@ -13,35 +13,43 @@ const Counter = () => {
       <br />
 
       <form action="">
-        <div>
-          <textarea
-            className="border-solid border-gray border-2 rounded mt-5 ml-5"
-            cols="80"
-            rows="5"
-            placeholder="e.g by gift tomorrow at 6pm"
-          ></textarea>
-          {/* <textarea
-            nameName="border-none"
-            cols="60"
-            rows="10"
-            placeholder="description"
-          ></textarea> */}
-          <div class="action-btn">
-            <div class="right">
-              <button className="ml-5 bg-white text-[#16a34a] rounded border-gray border-2 h-18 w-14">
-                Today
-              </button>
-              <button className="ml-4 bg-[#f5f5f4] text-black border-gray border-2 h-18 w-14 rounded">
-                Inbox
-              </button>
+        {
+          <div className="w-50 h-50 p-5 rounded border-2 border-solid boder-gray m-10">
+            <div>
+              <input
+                placeholder="enter text"
+                className="w-full focus:outline-none"
+              />
             </div>
-            <div class="lefyt">
-              <span></span>
-              <span></span>
-              <span></span>
+            <div>
+              <input
+                placeholder="start your text"
+                className="w-full focus:outline-none"
+              />
+            </div>
+            <div className="flex justify-content:space-between">
+              <div>
+                <button className="mt-5 bg-white text-[#16a34a] rounded border-gray border-2 h-18 w-14">
+                  Today
+                </button>
+                <button className="mt-4 bg-[#f5f5f4] text-black border-gray border-2 h-18 w-14 rounded">
+                  Inbox
+                </button>
+              </div>
+              <div>
+                <span>
+                  <ion-icon name="subway-outline"></ion-icon>
+                </span>
+                <span>
+                  <ion-icon name="subway-outline"></ion-icon>
+                </span>
+                <span>
+                  <ion-icon name="subway-outline"></ion-icon>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
+        }
         <div class="start">
           <button className="border-2 border-gray bg-[#f87171] rounded ml-3">
             Addtask
